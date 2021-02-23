0721d188330cddbb2e3ebef58f1960170de9e9e1
99002591 <amit.das@ltts.com>
2021-02-23 17:19:44 +0530

    Reports

--- src/test/java/com/eaton/api/tests/v1/P1_SecurityTokenAPITests.java
+++ src/test/java/com/eaton/api/tests/v1/P1_SecurityTokenAPITests.java
@@ -13,7 +13,7 @@ public class P1_SecurityTokenAPITests {
 	public static ExtractableResponse<?> raw_response;
 	
 	// ***** //
-//	public static final int ALL_OK_CODE = 200;
+	public static final int ALL_OK_CODE = 200;
 	public static final String ALL_OK_LINE = "HTTP/1.1 200 OK";
 	// ***** //
 	
@@ -21,7 +21,7 @@ public class P1_SecurityTokenAPITests {
 	public static void successfullyLogin() {
 		raw_response = ParamApiObj.postToLogin(APIObj.API_1_SECURITY);
 		System.out.println(raw_response.body().asPrettyString());
-		assertEquals(raw_response.statusCode(), APIs.StatusCode.Ok);
+		assertEquals(raw_response.statusCode(), ALL_OK_CODE);
 		assertEquals(raw_response.statusLine(), ALL_OK_LINE);
 	}
 }
